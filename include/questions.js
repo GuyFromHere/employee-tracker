3e7108cda41c102208aa93c182e2f672a645628d
Dan Dismuke <dandismuke@gmail.com>
2020-01-15 22:12:28 -0800

    Simple inquirer logic in place. Getting and showing queries in response to prompt results.

--- include/questions.js
+++ include/questions.js
@@ -1,6 +1,24 @@
 module.exports = [
 	{
+		type: "list",
+		name: "action",
+		message: "What would you like to do?",
+		choices: [
+			"View All Employees",
+			"View All Employees By Department",
+			"View All Employees By Manager",
+			"Add Employee",
+			"Remove Employee",
+			"Update Employee Role",
+			"Update Employee Manager"
+		]
+	},
+	{
 		type: "input",
-		message: "Is it over?"
+		name: "first_name",
+		message: "Enter the Employee's first name:",
+		when: function(answers) {
+			return answers.action == "Add Employee";
+		}
 	}
 ];
--- index.js
+++ index.js
@@ -2,6 +2,29 @@ const inquirer = require("inquirer");
 const db_conn = require("./include/db");
 const questions = require("./include/questions");
 
+function processAnswers(answers) {
+	console.log("And your answers are:", answers);
+	if (answers.action == "View All Employees") {
+		db_conn.query(
+			"SELECT * FROM employeeTracker.employees",
+			(err, result) => {
+				if (err) throw err;
+				const stringResult = JSON.stringify(result);
+				const parsedResult = JSON.parse(stringResult);
+				parsedResult.forEach(item => {
+					console.log(item.first_name + " " + item.last_name);
+				});
+			}
+		);
+	} else if (answers.action == "Add Employee") {
+		console.log("Add " + answers.first_name + " to database.");
+	}
+}
+
 const run = function() {
-	return inquirer.prompt(questions).then(console.log("the end"));
+	return inquirer.prompt(questions).then(answers => {
+		processAnswers(answers);
+	});
 };
+
+run();
